7751ae5b46619286c151f68129f591411c02e6c4
Marcos Vinicius Maia <juninhowblu@gmail.com>
2017-11-04 20:52:08 -0200

    Added first project files :tada:

---
+++ NetworkUtils/NetworkUtils/NetworkDiscovery.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+namespace NetworkUtils
+{
+    public class NetworkDiscovery
+    {
+        //BASE CODE FROM https://stackoverflow.com/questions/13492134/find-all-ip-address-in-a-network
+
+        private List<Ping> Pingers { get; set; } = new List<Ping>();
+        private int Instances { get; set; }
+
+        private readonly object _lock = new object();
+        public string BaseIp { get; set; } = "192.168.1.";
+        private int TimeOut { get; set; } = 250;
+
+        private int Ttl { get; set; } = 5;
+
+        /// <summary>
+        /// Creates a new NetworkDiscovery instance
+        /// </summary>
+        /// <param name="baseIp">The base ip address mask. Ex: <b>192.168.1.</b> **MUST END IN A DOT**</param>
+        public NetworkDiscovery(string baseIp = null)
+        {
+            if (baseIp != null)
+                BaseIp = baseIp;
+        }
+
+        public async Task<List<IPAddress>> SearchAsync()
+        {
+            CreatePingers(255);
+
+            var po = new PingOptions(Ttl, true);
+            var enc = new System.Text.ASCIIEncoding();
+            var data = enc.GetBytes("abababababababababababababababab");
+
+            var found = new List<IPAddress>();
+            var tasks = Pingers.Select((p, o) =>
+                {
+                    return Task.Factory.StartNew(async () =>
+                    {
+                        lock (_lock)
+                        {
+                            Instances += 1;
+                        }
+                        var pingReply = await p.SendPingAsync($"{BaseIp}{o++}", TimeOut, data, po);
+
+                        if (pingReply.Status == IPStatus.Success)
+                            found.Add(pingReply.Address);
+                    });
+                })
+                .Cast<Task>()
+                .ToList();
+
+            await Task.WhenAll(tasks);
+
+            DestroyPingers();
+
+            return found;
+        }
+
+
+        private void CreatePingers(int cnt)
+        {
+            for (var i = 1; i <= cnt; i++)
+                Pingers.Add(new Ping());
+        }
+
+        private void DestroyPingers()
+        {
+            foreach (var p in Pingers)
+                p.Dispose();
+
+            Pingers.Clear();
+        }
+    }
+}
---
+++ NetworkUtils/NetworkUtils/NetworkExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace NetworkUtils
+{
+    public static class NetworkExtensions
+    {
+        public static Task<bool> PingHostAsync(this string hostUri, int portNumber)
+        {
+            return Task<bool>.Factory.StartNew(() =>
+            {
+                try
+                {
+                    var client = new TcpClient(hostUri, portNumber);
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            });
+        }
+
+        public static Task<bool> PingHostAsync(this IPAddress hostUri, int portNumber)
+        {
+            return PingHostAsync(hostUri.ToString(), portNumber);
+        }
+    }
+}
